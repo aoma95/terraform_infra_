f4d9a95b477271bbc894ff91455dc05f7a71ebbd
Dan ESPOSITO <espositodan95@gmail.com>
2023-01-21 13:22:32 +0000

    update name module instace to instance

--- modules/instace/outpout.tf
+++
@@ -1,8 +0,0 @@
-output "instance" {
-  value = {
-    name = scaleway_instance_server.instance.name
-    image = scaleway_instance_server.instance.image
-    type = scaleway_instance_server.instance.type
-  }
-  description = "instance generate"
-}
